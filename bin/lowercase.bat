fa13ecc105387b0eee9ae24486ecc2df1028b3f6
Jeffrey Lee <icmr@hotmail.com>
2016-02-24 14:13:52 +0800

    init

---
+++ bin/lowercase.bat
@@ -0,0 +1,15 @@
+rem $ bin/lowercase.bat test/data/sample.txt
+
+rem C:\git\nodejs-package>node.exe bin/lowercase test/data/sample.txt
+rem C:\git\nodejs-package\bin/../test/data/sample.txt
+rem java
+rem nodejs
+rem python
+rem php
+rem .net
+rem r
+rem ruby
+rem c
+rem c++
+rem go
+node.exe bin/lowercase %1
---
+++ example.js
@@ -0,0 +1,6 @@
+"use strict"
+
+var lowercase = require('./lowercase.js');
+var myfile = "test/data/sample.txt";
+
+lowercase.lowerCase(myfile);
